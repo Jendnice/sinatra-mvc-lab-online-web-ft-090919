d3fdba340e7834635dcd9319bcb6513c005fc8c9
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-25 21:03:18 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,36 +1,36 @@
 
-class PigLatinizer 
+# class PigLatinizer 
   
-  attr_accessor :string
+#   attr_accessor :string
   
-  def initialize(string)
-    @string = string.downcase 
-  end 
+#   def initialize(string)
+#     @string = string.downcase 
+#   end 
   
-  def piglatinize
-    self.string = string 
-    a = string.split(" ")
-    b = a.map {|word| piglatinize_word(word)}
-    b.join(" ")
-  end
+#   def piglatinize
+#     self.string = string 
+#     a = string.split(" ")
+#     b = a.map {|word| piglatinize_word(word)}
+#     b.join(" ")
+#   end
   
-  def piglatinize_word(word)
-    first_letter = word[0].downcase
-    if ["a", "e", "i", "o", "u"].include?(first_letter)
-        "#{word}way"
-    else
-      consonants = []
-      consonants << word[0]
-        if ["a", "e", "i", "o", "u"].include?(word[1]) == false
-          consonants << word[1]
-          if ["a", "e", "i", "o", "u"].include?(word[2]) == false
-            consonants << word[2]
-          end
-        end
-      "#{word[consonants.length..-1] + consonants.join + "ay"}"
-    end
-  end
+#   def piglatinize_word(word)
+#     first_letter = word[0].downcase
+#     if ["a", "e", "i", "o", "u"].include?(first_letter)
+#         "#{word}way"
+#     else
+#       consonants = []
+#       consonants << word[0]
+#         if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+#           consonants << word[1]
+#           if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+#             consonants << word[2]
+#           end
+#         end
+#       "#{word[consonants.length..-1] + consonants.join + "ay"}"
+#     end
+#   end
 
-end 
+# end 
 
 
